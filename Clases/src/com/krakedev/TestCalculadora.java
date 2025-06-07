36bfd34038bdc3386ce9233c14b286811a708b76
bpgualotuna <bpgualotuna@espe.edu.ec>
2025-06-07 13:00:15 -0500

    Reto 9, invocacion funciones y pruebas

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -7,10 +7,19 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		calcu = new Calculadora();
-		resultadoSuma = calcu.sumar(2, 3);
-		int resultadoResta = calcu.restar(5, 2);
-		System.out.println("Resultado: " + resultadoSuma);
-		System.out.println("Resultado: " + resultadoResta);
+		resultadoSuma = calcu.sumar(5, 3);
+		int resultadoResta = calcu.restar(5, 3);
+		double resultadoMultiplicar = calcu.multiplicar(5, 3);
+		double resultadoDividir = calcu.dividir(5, 3);
+		double resultadoPromedio = calcu.promediar(5, 7, 6);
+		System.out.println("Resultado Suma: " + resultadoSuma);
+		System.out.println("Resultado Resta: " + resultadoResta);
+		System.out.println("Resultado Multiplicacion: " + resultadoMultiplicar);
+		System.out.println("Resultado Division: " + resultadoDividir);
+		System.out.println("Resultado Promedio: " + resultadoPromedio);
+		System.out.println("Resultado Mostrar: ");
+		calcu.mostrarResultado();
+		
 		
 	}
 
