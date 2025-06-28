6bb6fb041856da4ebdeb8addc7f63059f4abcb5f
bpgualotuna <bpgualotuna@espe.edu.ec>
2025-06-28 11:27:07 -0500

    Reto30

---
+++ ejemplomaven/src/main/java/com/krakedev/ejemplomaven/Admin.java
@@ -0,0 +1,21 @@
+package com.krakedev.ejemplomaven;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private static Logger logger = LogManager.getLogger(Admin.class);
+	
+	public void agregar() {
+		
+		logger.warn("mensaje de warn");
+		logger.info("mensaje de info");
+		logger.debug("mensaje de debug");
+		logger.trace("mensaje de trace");
+		
+		logger.error("mensaje de error");
+		
+		
+	}
+	
+}
